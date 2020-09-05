bfa466313f46dc83202df1f686645c5e773aa4d0
Marlin Schäfer <mb.schaefer@gmx.de>
2020-09-05 19:58:15 +0200

    Added YahooFeed.
    Changes:
    -Small bug-fixes
    -Renamed BaseCandleFeed to CandleFeed
    -Added a class-method to CandleFeed to construct it from a
     pandas.DataFrame
    -Added the YahooFeed class that utilizes the yfinance package to
     obtain financial data
    -Added a plotting function to CandleFeed that plots the Candles
     from the feed.

--- feed/basefeed.py
+++ feed/basefeed.py
@@ -1,17 +1,21 @@
 from PyTrest.types import DateSeries, Candle
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+import datetime
+import yfinance as yf
 
-class BaseCandleFeed(DateSeries):
+class CandleFeed(DateSeries):
     def __init__(self, name='N/A', currency='USD', data=None, index=None,
                  datetime_format='%d.%m.%Y %H:%M:%S'):
-        super.__init__(data=data, index=index,
+        super().__init__(data=data, index=index,
                        datetime_format=datetime_format)
         self.name = name
         self.currency = currency
     
-    def add_candle(self, dateindex, candle):
+    def add_candle(self, dateindex, candle, names=None):
         if isinstance(candle, Candle):
             if candle.currency != self.currency:
-                candle = candle.covnert(self.currency)
+                candle = candle.convert(self.currency)
             if candle.timestamp is None:
                 candle.timestamp = dateindex
             elif candle.timestamp != dateindex:
@@ -19,7 +23,7 @@ class BaseCandleFeed(DateSeries):
             self.insert_value(dateindex, value=candle)
         else:
             candle = Candle(data=candle, currency=self.currency,
-                            timestamp=dateindex)
+                            timestamp=dateindex, names=names)
             self.insert_value(dateindex, value=candle)
     
     @property
@@ -62,4 +66,56 @@ class BaseCandleFeed(DateSeries):
         return DateSeries(data=data, index=self.index,
                           datetime_format=self.datetime_format)
     vol = volume
-    pass
+    
+    @classmethod
+    def from_dataframe(cls, dataframe, name='N/A', currency='USD',
+                       datetime_format='%d.%m.%Y %H:%M:%S', names=None):
+        if names is None:
+            names = Candle().names
+        
+        index = [date.to_pydatetime() for date in dataframe.index]
+        data = []
+        for i in range(len(dataframe)):
+            data.append(Candle(data=dict(dataframe.iloc[i]),
+                               currency=currency,
+                               timestamp=index[i],
+                               names=names))
+        return CandleFeed(name=name, currency=currency, data=data,
+                          index=index, datetime_format=datetime_format)
+    
+    def plot(self):
+        fig, ax = plt.subplots()
+        width = datetime.timedelta(days=0.5)
+        for date, candle in zip(self.index, self.data):
+            if candle.open < candle.close:
+                color = 'green'
+            else:
+                color = 'red'
+            ax.plot([date, date],
+                    [float(candle.low), float(candle.high)],
+                    color=color)
+            height = float(candle.close - candle.open)
+            ax.add_patch(Rectangle((date-width/2, float(candle.close)),
+                                   width,
+                                   height,
+                                   color=color))
+        plt.show()
+
+class YahooFeed(CandleFeed):
+    def __init__(self, ticker, currency='USD',
+                 datetime_format='%d.%m.%Y %H:%M:%S', **kwargs):
+        ticker = yf.Ticker(ticker)
+        if 'period' not in kwargs:
+            kwargs['period'] = 'max'
+        dataframe = ticker.history(**kwargs)
+        
+        names = Candle().names
+        index = [date.to_pydatetime() for date in dataframe.index]
+        data = []
+        for i in range(len(dataframe)):
+            data.append(Candle(data=dict(dataframe.iloc[i]),
+                               currency=currency,
+                               timestamp=index[i],
+                               names=names))
+        super().__init__(name=str(ticker), currency=currency, data=data,
+                          index=index, datetime_format=datetime_format)
