2f78fc3812ddd381ce81a9a6f9fe87291e4a1ce8
Marlin Schäfer <mb.schaefer@gmx.de>
2022-08-20 19:01:36 +0200

    Parallel update

--- utils/update.py
+++ utils/update.py
@@ -1,4 +1,5 @@
 from ..feed import YahooFeed, CandleFeed
+from ..types.events import Event
 import datetime
 
 def update_yahoo_feed(feed, dateindex=None):
@@ -15,3 +16,30 @@ def update_yahoo_feed(feed, dateindex=None):
         else:
             feed.add_candle(date, candle)
     return
+
+
+def update_feed(feed, dateindex=None):
+    if dateindex is None:
+        dateindex = datetime.datetime.now()
+    
+    start = feed.max_dateindex - datetime.timedelta(days=7)
+    end = dateindex
+    down_data = YahooFeed(feed.name, start=start, end=end)
+    
+    added = 0
+    for date, candle in zip(down_data.index, down_data.data):
+        if date in feed:
+            continue
+        else:
+            feed.index.append(date)
+            feed.data.append(candle)
+            added += 1
+    if added > 0 and hasattr(feed, 'handler'):
+        feed.handler.events += 1
+        eventid = feed.handler.events
+        event = Event(event_tag='insert_value',
+                      emitter=feed,
+                      event_id=eventid,
+                      args=[feed, feed.index[-1]],
+                      kwargs={'value': feed.data[-1]})
+        feed.handler.handle_event(event)
